46bb733e21269d60c6c037ebda639052abc1ba4b
Yunfei Zhang <beethovenzhang@gmail.com>
2019-06-10 21:22:58 -0700

    connect to MongoDB using Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connect DB
+connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
