4b35e6ccfaa2e6ce14db7088676e19367f47bf72
Yunfei Zhang <beethovenzhang@gmail.com>
2019-06-14 21:51:19 -0700

    test routes with Postman and fix minor bugs

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 
 // @route GET api/auth
 // @desc Test route
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 
 // @route GET api/posts
 // @desc Test route
