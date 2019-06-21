c250a2d761dd93cebae1220fb6ea561fe3c46c21
Yunfei Zhang <beethovenzhang@gmail.com>
2019-06-20 20:08:02 -0700

    check if user exsits during registration

--- models/User.js
+++ models/User.js
@@ -23,4 +23,4 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-module.exports = User = mongoose.Schema('user', UserSchema);
+module.exports = User = mongoose.model('user', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
+const User = require('../../models/User');
+
 // @route POST api/users
 // @desc Register user
 // @access Public
@@ -17,12 +19,32 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exsits' }] });
+      }
+
+      // Get user's gravatar
+
+      // Encrypt password
+
+      // Return json webtoken
+      res.send('User route');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
