eda926826638f845b2cdfc059197182fb52c5904
Yunfei Zhang <beethovenzhang@gmail.com>
2019-06-21 20:42:15 -0700

    return a jwt for registered users

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../../models/User');
 
 // @route POST api/users
@@ -59,7 +61,23 @@ router.post(
       await user.save();
 
       // Return json webtoken
-      res.send('User registered');
+      // user.id : id of the user object in MongoDB
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
