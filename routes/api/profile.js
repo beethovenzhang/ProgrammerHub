b9dd87a6954c4d2bdd3e15d70b85f87c84daec64
Yunfei Zhang <beethovenzhang@gmail.com>
2019-06-24 21:14:23 -0700

    build profile object

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -64,6 +64,19 @@ router.post(
       instagram,
       linkedin
     } = req.body;
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
   }
 );
 
