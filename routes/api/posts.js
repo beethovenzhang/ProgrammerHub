56b1a8d601a24d959637c258fa7ddf8a3ae02e4f
Yunfei Zhang <beethovenzhang@gmail.com>
2019-06-29 11:22:05 -0700

    implement api for getting all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -43,4 +43,17 @@ router.post(
   }
 );
 
+// @route GET api/posts
+// @desc Get all posts
+// @access Private
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 module.exports = router;
