b41d23e469ac7328ee2ee7390c1a8826766dccb3
Yunfei Zhang <beethovenzhang@gmail.com>
2019-06-29 11:33:10 -0700

    implement get post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -56,4 +56,24 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @route GET api/posts/:id
+// @desc Get post by ID
+// @access Private
+
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.json(post);
+  } catch (err) {
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 module.exports = router;
