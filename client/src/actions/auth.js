8a0f7a892d928d89d3bfe6e7ef06e9954d773259
Yunfei Zhang <beethovenzhang@gmail.com>
2019-07-08 21:30:48 -0700

    connect the login component to actions and fix minor stuff

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -46,6 +46,7 @@ export const register = ({ name, email, password }) => async dispatch => {
       type: REGISTER_SUCCESS,
       payload: res.data
     });
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -75,6 +76,7 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_SUCCESS,
       payload: res.data
     });
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
