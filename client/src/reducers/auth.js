22924ca167c43dac34e6e0f6f6fe6ab516e5a437
Yunfei Zhang <beethovenzhang@gmail.com>
2019-07-08 21:06:23 -0700

    add login action and reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS
 } from '../actions/types';
 
 const initialState = {
@@ -23,10 +25,12 @@ export default function(state = initialState, action) {
         user: payload
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return { ...state, ...payload, isAuthenticated: true, loading: false };
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem('itme');
       return { ...state, token: null, isAuthenticated: false, loading: false };
     default:
