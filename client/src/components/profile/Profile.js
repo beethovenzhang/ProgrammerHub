cda1eff7fabbbf3b08caa7efe67265cf6a78e0af
Yunfei Zhang <beethovenzhang@gmail.com>
2019-07-27 22:23:32 -0700

    implement ProfileEducation component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import { getProfileByID } from '../../actions/profile';
 
 const Profile = ({
@@ -50,6 +51,22 @@ const Profile = ({
                 </Fragment>
               ) : (
                 <h4>No Experience Added</h4>
+              )}
+            </div>
+
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No Education Added</h4>
               )}
             </div>
           </div>
