6b9e590e56098c4f121b37add9a89898aa7b79f8
Yunfei Zhang <beethovenzhang@gmail.com>
2019-07-28 18:51:25 -0700

    implement addLike and removeLike in the PostItem component and fix bugs

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -19,9 +19,9 @@ export const getPosts = () => async dispatch => {
   }
 };
 //Add like
-export const addLike = postId => async dispatch => {
+export const addLike = id => async dispatch => {
   try {
-    const res = await axios.put(`/api/posts/like/${postId}`);
+    const res = await axios.put(`/api/posts/like/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
@@ -30,15 +30,16 @@ export const addLike = postId => async dispatch => {
   } catch (err) {
     dispatch({
       type: POST_ERROR,
-      payload: { msg: err.response.data.msg, status: err.response.status }
+      payload: { msg: err.response.statusText, status: err.response.status }
     });
   }
 };
 
 //Remove like
-export const removeLike = postId => async dispatch => {
+// Takes in the postId
+export const removeLike = id => async dispatch => {
   try {
-    const res = await axios.put(`/api/posts/like/${postId}`);
+    const res = await axios.put(`/api/posts/unlike/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -26,7 +26,7 @@ export default function(state = initialState, action) {
     case UPDATE_LIKES:
       return {
         ...state,
-        posts: state.post.map(post =>
+        posts: state.posts.map(post =>
           post._id === payload.id ? { ...post, likes: payload.likes } : post
         ),
         loading: false
