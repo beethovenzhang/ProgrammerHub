c2e7dcc7d6ecba26623d815aec2138ddf63862cb
Yunfei Zhang <beethovenzhang@gmail.com>
2019-07-28 21:13:39 -0700

    implement deletePost button function

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -17,3 +17,5 @@ export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const POST_ERROR = 'POST_ERROR';
 export const GET_POSTS = 'GET_POSTS';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
+export const DELETE_POST = 'DELETE_POST';
+export const ADD_POST = 'ADD_POST';
