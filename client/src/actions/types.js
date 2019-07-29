6dcb9826bbfe4fc7d7a51b6235c249efeb06ba97
Yunfei Zhang <beethovenzhang@gmail.com>
2019-07-28 17:52:53 -0700

    implement UPDATE_LIKES action and reducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -11,6 +11,7 @@ export const GET_PROFILE = 'GET_PROFILE';
 export const GET_PROFILES = 'GET_PROFILES';
 export const GET_REPOS = 'GET_REPOS';
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
+export const UPDATE_LIKES = 'UPDATE_LIKES';
 export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const POST_ERROR = 'POST_ERROR';
