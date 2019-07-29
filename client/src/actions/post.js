9f1e067dcf85a4e141d7d65a55b00f15401631fd
Yunfei Zhang <beethovenzhang@gmail.com>
2019-07-28 22:16:30 -0700

    implement addComment and removeComment actions and reducer

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,7 +6,9 @@ import {
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
-  GET_POST
+  GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from './types';
 
 //Get posts
@@ -117,6 +119,53 @@ export const getPost = id => async dispatch => {
     dispatch({
       type: POST_ERROR,
       payload: { msg: err.response.data.msg, status: err.response.status }
+    });
+  }
+};
+
+//Add comment
+export const addComment = (postId, formData) => async dispatch => {
+  /*need config for sending data*/
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  try {
+    const res = await axios.post(
+      `/api/posts/comment/${postId}`,
+      formData,
+      config
+    );
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Comment Created', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId
+    });
+
+    dispatch(setAlert('Comment Deleted', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
     });
   }
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -20,3 +20,5 @@ export const GET_POST = 'GET_POST';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 export const DELETE_POST = 'DELETE_POST';
 export const ADD_POST = 'ADD_POST';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
